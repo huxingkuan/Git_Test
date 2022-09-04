be9ac324b3772d08a903ce5a6750bc187aeb31cf
胡行宽 <xingkuanhu@gmail.com>
2022-09-04 23:07:38 +0800

    测试patch有效性

--- src/Git.java
+++ src/Git.java
@@ -2,5 +2,8 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("测试git第一版");
         System.out.println("测试一下patch文件");
+        System.out.println("测试一下patch文件");
+        System.out.println("测试一下patch文件");
+        System.out.println("测试一下patch文件");
     }
 }
